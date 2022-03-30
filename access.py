ab5e806111127b69b4dcd45910a438b183a7a3fc
Nathan Shulman <60013163+nshulman25@users.noreply.github.com>
2022-03-29 23:08:14 -0400

    Add files via upload

--- access.py
+++ access.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python
 import sys
 
+# clears all text files before execution
 open("accounts.txt", "w").close()
+open("audits.txt", "w").close()
+open("groups.txt", "w").close()
+open("files.txt", "w").close()
 
+# variables
 users = []
 current_user = [0]
+groups = {}
+files = {}
 
 # Loops through the user specified file line by line and adds each line to a list. 
 file  = sys.argv[1]
 lines = []
 with open(file) as f:
 	lines = f.readlines()
+
+# helper func to add to audit file	
+def audit(output):	
+	print(output)
+	f = open("audits.txt" ,"a")
+	f.write(output + "\n")
+	f.close()	
+
+# terminates if useradd root password is not first command
+if "useradd root" not in lines[0]:
+	audit("The first command must be in the form \"useradd root password\"")
+	quit()
+
 
 # adds new user 
 def useradd(username, password):
@@ -19,44 +39,125 @@ def useradd(username, password):
 		f.write(username + " " + password+ "\n")
 		f.close()
 		users.append(username)
+		audit("user " + username + " created")
+		
 	elif current_user[0] == "root":
-		f = open("accounts.txt" ,"a")
-		f.write(username + " " + password+ "\n")
-		f.close()
-		users.append(username)
+		if username in users:
+			audit("That username is take already")
+		else:	
+			f = open("accounts.txt" ,"a")
+			f.write(username + " " + password+ "\n")
+			f.close()
+			users.append(username)
+			audit("user " + username + " created")
 	else:
-		print("Only the root user can add new users!")	
-	
-	
+		audit("Only the root user can add new users!")	
 
+# logs in specified user. 
+# username and password must match in the database
 def login(username, password):
 	if current_user[0]:
-		print("The current user must be logged out before you can login!")
+		audit("The current user must be logged out before you can login!")
 	else:
 		with open("accounts.txt") as f:
 			accounts = []
 			accounts = f.readlines()
 			for line in accounts:
 				user = line.split()
-				if(user[0]==username and user[1]==password):
-					current_user[0] = username
-					print(current_user[0])
-				else:
-					print("username or password is wrong!")
+				if(user[0]==username):
+					if(user[1]==password):
+						current_user[0] = username
+						audit(current_user[0] + " has logged in")						
+					else:
+						audit("username or password is wrong!")
+
+# logsout the current user					
 def logout():
-	current_user[0] = ""
+	if "" in current_user:
+		audit("There is currently no user signed in")
+	else:
+		audit(current_user[0] + " has logged out")
+		current_user[0] = ""
 
+# creates group of the specific name
+# {groupname: [list of members]}
 def groupadd(groupname):
-	pass
-
+	if current_user[0] == "root":
+		if groupname in groups:
+			audit("That group already exsists")			
+		if "nil" in groups:
+			audit("nil is an invalid group name")
+		else:
+			groups[groupname] = []
+			audit("group " + groupname + " created")
+	else:
+		audit("You do not have root access")
+	
+# add the specific user to the specific group
 def usergrp(username, groupname):
-	pass
+	if current_user[0] == "root":
+		if username not in users:
+			audit(username + " is not a valid user")
+		if groupname not in groups:
+			audit(groupname + " is not a valid group")	
+		else:
+			groups[groupname] += [username]
+			audit(username + " added to " + groupname)
+	else:
+		audit("You do not have root access")
 
+# creates a file of the specified name with default permssions
+# {filename: [ownername, groupname, owner, group, other]}
 def mkfile(filename):
-	pass
+	if current_user[0] != "":
+		if filename in files:
+			audit("That file already exsists")			
+		elif filename == "accounts.txt":
+			audit("accounts.txt is a reserved filename")
+		elif filename == "audits.txt":
+			audit("audits.txt is a reserved filename")
+		elif filename == "groups.txt":
+			audit("groups.txt is a reserved filename")
+		elif filename == "files.txt":
+			audit("files.txt is a reserved filename")			
+		else:
+			files[filename] = [current_user[0], "nil", "rw-", "---", "---"]
+			#f = open(filename ,"a")
+			#f.close()
+			audit("file " + filename + " created")
+	else:
+		audit("A user must be signed in to create a file")
+
 	
 def chmod(filename, owner, group, others):
-	pass
+	tempgroup = files[filename][1]
+	if filename in files:
+		
+		if len(owner) !=3 or len(group) !=3 or len(others) !=3:
+			audit("access permission can only be 3 chars")
+		elif owner[0] != "r" and owner[0] != "-":
+			audit("the first letter of a permission must be \"r\" or \"-\"")
+		elif group[0] != "r" and group[0] != "-":
+			audit("the first letter of a permission must be \"r\" or \"-\"")
+		elif others[0] != "r" and others[0] != "-":
+			audit("the first letter of a permission must be \"r\" or \"-\"")
+		elif owner[1] != "w" and owner[1] != "-":
+			audit("the second letter of a permission must be \"w\" or \"-\"")
+		elif group[1] != "w" and group[1] != "-":
+			audit("the second letter of a permission must be \"w\" or \"-\"")
+		elif others[1] != "w" and others[1] != "-":
+			audit("the second letter of a permission must be \"w\" or \"-\"")
+		elif owner[2] != "x" and owner[2] != "-":
+			audit("the third letter of a permission must be \"x\" or \"-\"")
+		elif group[2] != "x" and group[2] != "-":
+			audit("the third letter of a permission must be \"x\" or \"-\"")
+		elif others[2] != "x" and others[2] != "-":
+			audit("the third letter of a permission must be \"x\" or \"-\"")
+	else:
+		print("That file does not exsist or is not editable")	
+	
+	files[filename] = [current_user[0], tempgroup, owner, group, others]
+	print(files[filename])
 
 def chown(filename, username):
 	pass		
@@ -77,16 +178,53 @@ def ls(filename):
 	pass
 
 def end():
-	pass
+	audit("Ending session")
+	with open("groups.txt" ,"a") as f:
+		for key,values in groups.items():
+			f.write(key + ": ")
+			for v in values:
+				f.write(v + " ")
+			f.write("\n")			
+	f.close()
 	
+	with open("files.txt" ,"a") as f:
+		for key,values in files.items():
+			f.write(key + ": ")
+			for v in values:
+				f.write(v + " ")
+			f.write("\n")	
+	f.close()
+	
+	
+# password check
+def passwordcheck(password):
+	if len(password) > 30:
+		audit("The password must be less then 30 characters")
+		return 1
+	else:
+		pass
 
+# name check
+def namecheck(name):
+	if len(name) > 30:
+		audit("The name must be less then 30 characters")
+		return 1
+	if "/" in name or ":" in name:
+		audit("The password must be less then 30 characters")
+		return 1
+	else:
+		pass		
+		
 # Loops through the list of lines split on whitespace.
 count = 0
 for line in lines:
 	command = line.split()
 
 	if(command[0] == "useradd"):
-		useradd(command[1], command[2])
+		if passwordcheck(command[2]) == 1 or namecheck(command[1]) == 1:
+			break
+		else:
+			useradd(command[1], command[2])
 		
 	if(command[0] == "login"):
 		login(command[1], command[2])
@@ -95,13 +233,19 @@ for line in lines:
 		logout()
 		
 	if(command[0] == "groupadd"):
-		groupadd(command[1])
+		if namecheck(command[1]) == 1:
+			break
+		else:
+			groupadd(command[1])
 		
 	if(command[0] == "usergrp"):
 		usergrp(command[1], command[2])
 		
 	if(command[0] == "mkfile"):
-		mkfile(command[1])
+		if namecheck(command[1]) == 1:
+			break
+		else:
+			mkfile(command[1])
 		
 	if(command[0] == "chmod"):
 		chmod(command[1], command[2], command[3], command[4])
@@ -125,5 +269,5 @@ for line in lines:
 		ls(command[1])
 		
 	if(command[0] == "end"):
-		end(command[1])	
+		end()	
 	
